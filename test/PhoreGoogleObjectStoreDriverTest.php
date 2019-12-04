dba3bf3a093b73528c6c40729922cf710025f169
Jan <jan@talpa-solutions.com>
2019-12-04 08:46:56 +0100

    fixed tests

--- test/PhoreGoogleObjectStoreDriverTest.php
+++ test/PhoreGoogleObjectStoreDriverTest.php
@@ -11,53 +11,62 @@ use PHPUnit\Framework\TestCase;
 class PhoreGoogleObjectStoreDriverTest extends TestCase
 {
 
-    private $configPath;
-    private $driver;
+    protected static $configPath;
+    /**
+     * @var PhoreGoogleObjectStoreDriver $driver
+     */
+    protected static $driver;
 
-    protected function setUp(): void
+    public static function setUpBeforeClass(): void
     {
-        $this->configPath = "/run/secrets/google_test";
-        $this->driver = new PhoreGoogleObjectStoreDriver($this->configPath, "phore-objectstore-unit-testing");
+        self::$configPath = "/run/secrets/google_test";
+        self::$driver = new PhoreGoogleObjectStoreDriver(self::$configPath, "phore-objectstore-unit-testing");
+    }
+
+    public static function tearDownAfterClass(): void
+    {
+        self::$driver->remove("testMeta.txt");
+        self::$driver->remove("testMetaRenamed.txt");
     }
 
     public function testHas()
     {
 
-        $this->assertFalse($this->driver->has("fail"));
+        $this->assertFalse(self::$driver->has("fail"));
     }
 
     public function testPutWithoutMeta()
     {
-        $this->driver->put("test.txt", "test");
-        $this->assertTrue($this->driver->has("test.txt"));
+        self::$driver->put("test.txt", "test");
+        $this->assertTrue(self::$driver->has("test.txt"));
     }
 
     public function testPutWithMeta()
     {
         $meta['testdata'] = "test";
-        $this->driver->put("testMeta.txt", "test", $meta);
+        self::$driver->put("testMeta.txt", "test", $meta);
 
-        $meta = $this->driver->getMeta("testMeta.txt");
+        $meta = self::$driver->getMeta("testMeta.txt");
         $this->assertEquals("test", $meta['metadata']['testdata']);
     }
 
     public function testGetMetaOfExisting()
     {
-        $meta = $this->driver->getMeta("DO_NOT_TOUCH_test_2019-12-02.txt");
+        $meta = self::$driver->getMeta("DO_NOT_TOUCH_test_2019-12-02.txt");
 
         $this->assertEquals("2019-12-02T16:20:41.732Z", $meta['timeCreated']);
     }
 
     public function testGetMetaOfNonExisting()
     {
-        $meta = $this->driver->getMeta("fail");
+        $meta = self::$driver->getMeta("fail");
 
         $this->assertEmpty($meta);
     }
 
     public function testGetExisting()
     {
-        $objectContent = $this->driver->get("DO_NOT_TOUCH_test_2019-12-02.txt");
+        $objectContent = self::$driver->get("DO_NOT_TOUCH_test_2019-12-02.txt");
 
         $this->assertEquals("DO NOT DELETE OR UPDATE", $objectContent);
     }
@@ -66,12 +75,12 @@ class PhoreGoogleObjectStoreDriverTest extends TestCase
     {
         $this->expectException(NotFoundException::class);
         $this->expectExceptionMessage("No such object");
-        $this->driver->get("fail");
+        self::$driver->get("fail");
     }
 
     public function testDeleteExisting()
     {
-        $result = $this->driver->remove("testMeta.txt");
+        $result = self::$driver->remove("testMeta.txt");
         $this->assertEmpty($result);
     }
 
@@ -79,25 +88,25 @@ class PhoreGoogleObjectStoreDriverTest extends TestCase
     {
         $this->expectException(NotFoundException::class);
         $this->expectExceptionMessage("No such object");
-        $this->driver->remove("fail");
+        self::$driver->remove("fail");
     }
 
     public function testAppendToNonExisting ()
     {
-        $result = $this->driver->append("testMeta.txt", "test append");
+        $result = self::$driver->append("testMeta.txt", "test append");
         $this->assertTrue($result);
     }
 
     public function testAppendToExisting ()
     {
-        $result = $this->driver->append("testMeta.txt", "test append 2");
+        $result = self::$driver->append("testMeta.txt", "test append 2");
         $this->assertIsArray($result);
         $this->assertEquals( "testMeta.txt", $result['name']);
     }
 
     public function testRenameExistingToNonExisting()
     {
-        $result = $this->driver->rename("testMeta.txt", "testMetaRenamed.txt");
+        $result = self::$driver->rename("testMeta.txt", "testMetaRenamed.txt");
         $this->assertEquals( "testMetaRenamed.txt", $result['name']);
     }
 
@@ -105,23 +114,16 @@ class PhoreGoogleObjectStoreDriverTest extends TestCase
     {
         $this->expectException(\InvalidArgumentException::class);
         $this->expectExceptionMessage("Cannot rename");
-        $result = $this->driver->rename("testMetaRenamed.txt", "test.txt");
+        self::$driver->rename("testMetaRenamed.txt", "test.txt");
     }
 
     public function testRenameNonExisting()
     {
         $this->expectException(PhoreHttpRequestException::class);
         $this->expectExceptionMessage("No such object");
-        $result = $this->driver->rename("fail", "something");
+        $result = self::$driver->rename("fail", "something");
         $this->assertEquals( "testMetaRenamed.txt", $result['name']);
     }
-
-    public function testCleanUpAfterTests()
-    {
-        $this->driver->remove("testMeta.txt");
-        $this->driver->remove("testMetaRenamed.txt");
-    }
-
 
 
 }
