c4d088f0e667617d8d6bedd7c10307979c0fe287
Matthias <m@tth.es>
2019-12-04 17:25:54 +0100

    test

--- test/async.php
+++ test/async.php
@@ -17,6 +17,7 @@ require __DIR__ . "/../vendor/autoload.php";
 
 $googleSecret = "/run/secrets/google_test";
 $driver = new PhoreGoogleObjectStoreDriver($googleSecret, "phore-objectstore-unit-testing");
+$driver = new PhoreGoogleObjectStoreDriver($googleSecret, "phore-test2");
 
 
 $queue = new PhoreHttpAsyncQueue();
@@ -30,7 +31,9 @@ $ok = 0;
 $token = $driver->accessToken;
 
 
-for ($i=0; $i<1000; $i++) {
+$driver->put("/test.dat", "some content");
+
+for ($i=0; $i<100; $i++) {
     $queue->queue(phore_http_request("https://storage.googleapis.com/storage/v1/b/phore-objectstore-unit-testing/o/DO_NOT_TOUCH_test_2019-12-02.txt")
         ->withBearerAuth($token)->withTimeout(10,100))->then(
         function(PhoreHttpResponse $response) use (&$data, &$ok)  {
