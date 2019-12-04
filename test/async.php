6a2abad1b9f51a091c58a6097b8513237331d964
Matthias <m@tth.es>
2019-12-04 17:39:24 +0100

    update

--- test/async.php
+++ test/async.php
@@ -16,40 +16,53 @@ use Phore\ObjectStore\Driver\PhoreGoogleObjectStoreDriver;
 require __DIR__ . "/../vendor/autoload.php";
 
 $googleSecret = "/run/secrets/google_test";
-$driver = new PhoreGoogleObjectStoreDriver($googleSecret, "phore-objectstore-unit-testing");
+
+
+
 $driver = new PhoreGoogleObjectStoreDriver($googleSecret, "phore-test2");
+$url = "https://storage.googleapis.com/storage/v1/b/phore-test2/o/test.dat";
+
+
+#$driver = new PhoreGoogleObjectStoreDriver($googleSecret, "phore-objectstore-unit-testing");
+#$url = "https://storage.googleapis.com/storage/v1/b/phore-objectstore-unit-testing/o/test.dat";
 
 
 $queue = new PhoreHttpAsyncQueue();
 
 //$queue->queue(phore_http_request("http://localhost/test.php?case=wait"));
 
-phore_out("Start");
+
 
 $err = 0;
 $ok = 0;
 $token = $driver->accessToken;
 
 
+
+
 $driver->put("test.dat", "some content");
 
-for ($i=0; $i<100; $i++) {
-    $queue->queue(phore_http_request("https://storage.googleapis.com/storage/v1/b/phore-test2/o/test.dat")
-        ->withBearerAuth($token)->withTimeout(10,100))->then(
-        function(PhoreHttpResponse $response) use (&$data, &$ok)  {
-            phore_out("OK$ok:");
-            $ok++;
+for ($iv=0; $iv<10; $iv++) {
+    phore_out("Start $iv");
+    for ($i = 0; $i < 500; $i++) {
+        $queue->queue(phore_http_request($url)
+            ->withBearerAuth($token)->withTimeout(10, 100))->then(
+            function (PhoreHttpResponse $response) use (&$data, &$ok) {
+                #phore_out("OK$ok:");
+                $ok++;
 
-        },
-        function (PhoreHttpRequestException $ex) use (&$err){
-            phore_out("ERR$err:" . $ex->getMessage());
-            $err++;
-        });
+            },
+            function (PhoreHttpRequestException $ex) use (&$err) {
+                phore_out("ERR$err:" . $ex->getMessage());
+                $err++;
+            });
+    }
+
+    $queue->wait();
+    phore_out("\nOK: $ok Err: $err\n");
 }
 
-$queue->wait();
 
-phore_out("\nOK: $ok Err: $err\n");
 
 
 
