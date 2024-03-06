82ba079bbecdd51e1d182422f395b0e74e9d9f03
Matthias Leuffen <matthes@leuffen.de>
2024-03-06 20:35:21 +0100

    update

--- src/Encryption/SodiumSyncEncryption.php
+++ src/Encryption/SodiumSyncEncryption.php
@@ -17,6 +17,7 @@ class SodiumSyncEncryption implements ObjectStoreEncryption
 
     public function encrypt(string $data): string
     {
+        $data = gzdeflate($data, 9); // Compress data (level 9)
         $nonce = random_bytes(SODIUM_CRYPTO_SECRETBOX_NONCEBYTES);
         $encrypted = sodium_crypto_secretbox($data, $nonce, $this->key);
         return $nonce . $encrypted;
@@ -30,7 +31,12 @@ class SodiumSyncEncryption implements ObjectStoreEncryption
         if ($decrypted === false) {
             throw new \InvalidArgumentException("Could not decrypt data");
         }
-        return $decrypted;
+        $deflatedData = gzinflate($decrypted);
+        if ($deflatedData === false) {
+            return $decrypted; // Transition period
+            throw new \InvalidArgumentException("Could not gzinflate data");
+        }
+        return $deflatedData;
     }
 
     public function supportsStreaming(): bool
